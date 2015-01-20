f5d555e7c9d423ff026b8f8c59035b664c986d49
Kenneth Blair <kenneth.blair@charter.com>
2015-01-20 14:41:20 -0700

    Updated package scan

--- src/main/java/net/kenblair/interview/c/SpringConfig.java
+++ src/main/java/net/kenblair/interview/c/SpringConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "net.kenblair.interview")
+@ComponentScan(basePackages = "net.kenblair.interview.c")
 public class SpringConfig {
 
 }
